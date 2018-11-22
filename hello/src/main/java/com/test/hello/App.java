33c09ead10a9441910a50b0095fef15122615edf
Forward233 <18729041176@163.com>
2018-11-22 08:46:52 +0800

    增加工具类

--- hello/src/main/java/com/test/hello/App.java
+++ hello/src/main/java/com/test/hello/App.java
@@ -6,7 +6,7 @@ package com.test.hello;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
 		System.out.println(Runtime.getRuntime().availableProcessors());
+		System.out.println("a".hashCode());
 	}
 }
